367f098fd739eb5a92406372704b47f5e942df3e
fpiaggio <fpiaggio92@gmail.com>
2018-10-19 18:31:35 -0300

    Updates

---
+++ precache-manifest.c87128a1a93132495d71e3460aeb380b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4eeb9d45f1af1a58d20",
+    "url": "/hackaton-1/static/js/runtime~main.b4eeb9d4.js"
+  },
+  {
+    "revision": "e0ed6593513ef73672a9",
+    "url": "/hackaton-1/static/js/main.e0ed6593.chunk.js"
+  },
+  {
+    "revision": "c33a5493f0901cb89c8e",
+    "url": "/hackaton-1/static/js/1.c33a5493.chunk.js"
+  },
+  {
+    "revision": "e0ed6593513ef73672a9",
+    "url": "/hackaton-1/static/css/main.461f4f28.chunk.css"
+  },
+  {
+    "revision": "cc0f4577694a7b3e0d13830c051762ea",
+    "url": "/hackaton-1/index.html"
+  }
+];
